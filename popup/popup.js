19133f03a405557d3db5f689946825bcee1761a0
subhradwi01 <subhradwi01@gmail.com>
2023-03-22 00:39:14 +0530

    State upadtin at frontend

--- popup/popup.js
+++ popup/popup.js
@@ -3,8 +3,49 @@ const startDateElem = document.getElementById("startDate");
 const endDateElem = document.getElementById("endDate");
 const startButtonElem = document.getElementById("startButton");
 const stopButtonElem = document.getElementById("stopButton");
+const runningElem = document.getElementById("running")
+const stoppedElem = document.getElementById("stopped")
+
+
+const hideElem = (elem) =>{
+    elem.style.display="none"
+}
+
+const showElem = (elem) =>{
+    elem.style.display=""
+}
+
+const disableElem = (elem) =>{
+    elem.disabled = true
+}
+
+const enabledElem = (elem) => {
+    elem.disabled=false
+}
+
+const handleOnStartState = () =>{
+    // Status span
+    showElem(runningElem)
+    hideElem(stoppedElem)
+
+    // Button
+    disableElem(startButtonElem)
+    enabledElem(stopButtonElem)
+}
+
+const handleOnStopState = () =>{
+    //Status span
+    showElem(stoppedElem)
+    hideElem(runningElem)
+
+    //Button
+    disableElem(stopButtonElem)
+    enabledElem(startButtonElem)
+} 
+
 
 startButtonElem.onclick = () =>{
+    handleOnStartState();
     const prefs ={
        locationId : locationIdElem.value,
        startDate: startDateElem.value,
@@ -14,12 +55,13 @@ startButtonElem.onclick = () =>{
     
 }
 stopButtonElem.onclick = () =>{
+    handleOnStopState();
     chrome.runtime.sendMessage({event:'onStop',})
     const endDate = endDateElem.value;
 }
 
 chrome.storage.local.get(["loactionId","startDate","endDate","isRunning"],(results)=>{
-    const {locationId,startDate,endDate}=results;
+    const {locationId,startDate,endDate,isRunning}=results;
     console.log(results)
     if(locationId){
         locationIdElem.value=locationId
@@ -31,5 +73,11 @@ chrome.storage.local.get(["loactionId","startDate","endDate","isRunning"],(resul
         endDateElem.value=endDate
     }
 
-    console.log("Running Status: ",results.isRunning);
+    if(isRunning){
+        handleOnStartState()
+    }else{
+        handleOnStopState()
+    }
+
 })
+
